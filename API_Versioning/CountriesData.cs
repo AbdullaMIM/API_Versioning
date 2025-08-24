70dae4e48a82c6a99112224c04b7aa4cf1313884
Mohamed Abdulla <Abdulhez10@gmail.com>
2025-08-24 09:06:20 +0530

    dev-Versioning-1 - Versioning (Version by Folder)

---
+++ API_Versioning/CountriesData.cs
@@ -0,0 +1,54 @@
+﻿using API_Versioning.Models.Domain;
+
+namespace API_Versioning
+{
+    public static class CountriesData
+    {
+        public static List<Country> Get()
+        {
+            return new List<Country>
+            {
+                new Country { Id = 1, Name = "United States" },
+                new Country { Id = 2, Name = "Canada" },
+                new Country { Id = 3, Name = "Mexico" },
+                new Country { Id = 4, Name = "United Kingdom" },
+                new Country { Id = 5, Name = "Germany" },
+                new Country { Id = 6, Name = "France" },
+                new Country { Id = 7, Name = "Italy" },
+                new Country { Id = 8, Name = "Spain" },
+                new Country { Id = 9, Name = "Australia" },
+                new Country { Id = 10, Name = "Japan" },
+                new Country { Id = 11, Name = "China" },
+                new Country { Id = 12, Name = "India" },
+                new Country { Id = 13, Name = "Brazil" },
+                new Country { Id = 14, Name = "South Africa" },
+                new Country { Id = 15, Name = "Russia" },
+                new Country { Id = 16, Name = "South Korea" },
+                new Country { Id = 17, Name = "Netherlands" },
+                new Country { Id = 18, Name = "Sweden" },
+                new Country { Id = 19, Name = "Norway" },
+                new Country { Id = 20, Name = "Finland" },
+                new Country { Id = 21, Name = "Denmark" },
+                new Country { Id = 22, Name = "Poland" },
+                new Country { Id = 23, Name = "Belgium" },
+                new Country { Id = 24, Name = "Switzerland" },
+                new Country { Id = 25, Name = "Austria" },
+                new Country { Id = 26, Name = "Ireland" },
+                new Country { Id = 27, Name = "Portugal" },
+                new Country { Id = 28, Name = "Greece" },
+                new Country { Id = 29, Name = "Turkey" },
+                new Country { Id = 30, Name = "UAE" },
+                new Country { Id = 31, Name = "Saudi Arabia" },
+                new Country { Id = 32, Name = "Argentina" },
+                new Country { Id = 33, Name = "Chile" },
+                new Country { Id = 34, Name = "Colombia" },
+                new Country { Id = 35, Name = "Peru" },
+                new Country { Id = 36, Name = "Venezuela" },
+                new Country { Id = 37, Name = "Egypt" },
+                new Country { Id = 38, Name = "Morocco" },
+                new Country { Id = 39, Name = "Kenya" },
+                new Country { Id = 40, Name = "Nigeria" }
+            };
+        }
+    }
+}
